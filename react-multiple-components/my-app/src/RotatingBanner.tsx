a3921f018a73296e194d4cf29e6c221430cba2e0
Shea Pone <sheapone@gmail.com>
2024-12-18 05:09:35 +0000

    Phase 3 react-multiple-components completed.

--- react-multiple-components/my-app/src/RotatingBanner.tsx
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -7,20 +7,43 @@ type Props = {
 type CountProps = {
   count: number;
   current: number;
+  onNumberClick: (index: number) => void;
 };
 
 type BannerProps = {
   item: string;
 };
 
+type ButtonClick = {
+  onClick: () => void;
+};
+
 export function RotatingBanner({ items }: Props) {
-  const [index /* setIndex */] = useState(0);
+  const [index, setIndex] = useState(0);
+
+  function handleIndicatorClick(indicatorIndex: number): void {
+    setIndex(indicatorIndex);
+  }
+
+  function handleNextClick(): void {
+    const currentIndex = index;
+    setIndex((currentIndex + 1) % items.length);
+  }
+
+  function handlePrevClick(): void {
+    const currentIndex = index;
+    setIndex((currentIndex - 1 + items.length) % items.length);
+  }
   return (
     <>
       <Banner item={items[index]} />
-      <PrevButton />
-      <Indicators count={items.length} current={index} />
-      <NextButton />
+      <PrevButton onClick={handlePrevClick} />
+      <Indicators
+        count={items.length}
+        current={index}
+        onNumberClick={handleIndicatorClick}
+      />
+      <NextButton onClick={handleNextClick} />
     </>
   );
 }
@@ -29,27 +52,28 @@ function Banner({ item }: BannerProps) {
   return <div>{item}</div>;
 }
 
-function NextButton() {
+function NextButton({ onClick }: ButtonClick) {
   return (
     <>
-      <button>Next</button>
+      <button onClick={onClick}>Next</button>
     </>
   );
 }
 
-function PrevButton() {
+function PrevButton({ onClick }: ButtonClick) {
   return (
     <>
-      <button>Prev</button>
+      <button onClick={onClick}>Prev</button>
     </>
   );
 }
 
-function Indicators({ count, current }: CountProps) {
+function Indicators({ count, current, onNumberClick }: CountProps) {
   const buttons = [];
   for (let i = 0; i < count; i++) {
     buttons.push(
       <button
+        onClick={() => onNumberClick(i)}
         style={{ backgroundColor: i === current ? 'lightblue' : 'white' }}>
         {i}
       </button>
