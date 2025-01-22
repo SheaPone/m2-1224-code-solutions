c81dbb7eaeb530bca33b9bcd1510dea7fec59fe8
Shea Pone <sheapone@gmail.com>
2025-01-22 03:34:22 +0000

    Completed unit-tests-jestexercise.

---
+++ unit-tests-jest/numbers.test.ts
@@ -0,0 +1,49 @@
+import { evenNumbers, toDollars, divideBy, multiplyBy } from './numbers';
+
+describe('evenNumbers', () => {
+  it('returns the even numbers', () => {
+    const numbers = [1, 4, 5, 10, 0];
+    const result = evenNumbers(numbers);
+    expect(result).toEqual([4, 10, 0]);
+  });
+});
+
+describe('toDollars', () => {
+  it('returns the number formatted in dollars and cents', () => {
+    const number = 123.456;
+    const result = toDollars(number);
+    expect(result).toEqual('$123.46');
+  });
+});
+
+describe('divideBy', () => {
+  it('Returns a new array of numbers where every entry has been divided by the given divisor', () => {
+    const numbers = [1, 4, 5, 10, 0];
+    const divisor = 2;
+    const result = divideBy(numbers, divisor);
+    expect(result).toEqual([0.5, 2, 2.5, 5, 0]);
+  });
+});
+
+describe('multiplyBy', () => {
+  it('Modifies an object by multiplying the value of each key, but only if that value is a number', () => {
+    const obj = { a: 2, b: 4, c: 'not a number', d: 10 };
+    const multiplier = 2;
+    const result = multiplyBy(obj, multiplier);
+    expect(result).toEqual({ a: 5, b: 9, c: 'not a number', d: 21 });
+  });
+
+  it('Returns an unchanged object if there are no numeric values', () => {
+    const obj = { a: 'hello', b: true, c: null };
+    const multiplier = 2;
+    const result = multiplyBy(obj, multiplier);
+    expect(result).toEqual({ a: 'hello', b: true, c: null });
+  });
+
+  it('Handles an empty object correctly', () => {
+    const obj = {};
+    const multiplier = 3;
+    const result = multiplyBy(obj, multiplier);
+    expect(result).toEqual({});
+  });
+});
